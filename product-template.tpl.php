fd67444c045e364b5916aa60b270d1d23f695e03
Tsvetelina Abadjieva <tsvetelina_abadjieva@abv.bg>
2018-04-22 20:23:34 +0300

    Example for product view

---
+++ product-template.tpl.php
@@ -0,0 +1,7 @@
+<p>
+	<strong><?php print($product_title); ?></strong><br>
+	<?php print($product_description); ?>
+	<span><?php print($product_description); ?></span><br>
+	<span><?php print($product_description);?></span><br>
+</p>
+
